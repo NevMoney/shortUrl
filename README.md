73335f64ab66fb2b7da08d3b1405dbe5480e2fc7
Nev <67838965+NevMoney@users.noreply.github.com>
2022-12-27 12:57:41 -0500

    mostly front end

--- README.md
+++ README.md
@@ -3,11 +3,32 @@
 Using Node.js and npm to create a short url service.
 
 [Dependencies](#dependencies)
-- express - web framework
-- yup - validation
-- monk - to talk to mongo database
-- helmet - security
-- morgan - logging
-- cors - cross origin resource sharing
-- dotenv - environment variables
-- nodemon - development server
+[+] express - web framework
+[+] yup - validation
+[+]  monk - to talk to mongo database
+[-] helmet - security
+[-] morgan - logging
+[-] cors - cross origin resource sharing
+[+] dotenv - environment variables
+[+] nodemon - development server
+[+] nanoid - generate random id
+
+[Front End](#front-end)
+- HTML
+- CSS
+- JavaScript
+
+[Back End](#back-end)
+- Node.js
+- Express.js
+- MongoDB
+
+[Deployment](#deployment)
+- MongoDB Atlas
+
+[Development](#development)
+- npm
+- nodemon
+- dotenv
+
+
---
+++ public/app.js
@@ -0,0 +1,42 @@
+console.log('app.js running')
+const button = document.getElementById('shrinkBtn')
+button.addEventListener('click', createUrl)
+
+const url = document.getElementById('url')
+const slug = document.getElementById('slug')
+const newUrl = document.getElementById('newUrl')
+const error = document.getElementById('error')
+
+async function createUrl(e) {
+  e.preventDefault()
+  const urlValue = url.value
+  const slugValue = slug.value
+  console.log('clicked', urlValue, slugValue)
+
+  // now we need to send this data to the server
+  const response = await fetch('/url', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      url: urlValue,
+      slug: slugValue || undefined,
+    }),
+  })
+  if (response.ok) {
+    console.log('response worked')
+    const data = await response.json()
+    console.log('data', data)
+    // append new url to the page to open in a new tab
+    newUrl.innerHTML = `<p><a href="${data.slug}" target="_blank">${window.location.origin}/${data.slug}</a></p>`
+  } else {
+    const data = await response.json()
+    //   first remove .hidden class
+    error.classList.remove('hidden')
+    //   then add the error message
+    error.innerHTML = `<p>${data.message}</p>`
+  }
+}
+
+console.log(window.location.origin)
