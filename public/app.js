08e678ad11db3636e378b9bcaed48f0f2aa982ec
Nev <67838965+NevMoney@users.noreply.github.com>
2023-01-02 11:48:09 -0500

    Update app.js
    ensuring logged in user can use front page to create urls

--- public/app.js
+++ public/app.js
@@ -13,28 +13,57 @@ async function createUrl(e) {
   const slugValue = slug.value
   console.log('clicked', urlValue, slugValue)
 
-  // now we need to send this data to the server
-  const response = await fetch('/url', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      url: urlValue,
-      slug: slugValue || undefined,
-    }),
-  })
-  if (response.ok) {
-    console.log('response worked')
-    const data = await response.json()
-    console.log('data', data)
-    // append new url to the page to open in a new tab
-    newUrl.innerHTML = `<p><a href="${data.slug}" target="_blank">${window.location.origin}/${data.slug}</a></p>`
+  const loggedIn = isLoggedIn()
+  const userId = localStorage.getItem('userId')
+
+  if (!loggedIn) {
+    // send this data to the server
+    const response = await fetch('/url', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        url: urlValue,
+        slug: slugValue || undefined,
+      }),
+    })
+    if (response.ok) {
+      console.log('response worked')
+      const data = await response.json()
+      console.log('data', data)
+      // append new url to the page to open in a new tab
+      newUrl.innerHTML = `<p><a href="${data.slug}" target="_blank">${window.location.origin}/${data.slug}</a></p>`
+    } else {
+      const data = await response.json()
+      //   first remove .hidden class
+      error.classList.remove('hidden')
+      //   then add the error message
+      error.innerHTML = `<p>${data.message}</p>`
+    }
   } else {
-    const data = await response.json()
-    //   first remove .hidden class
-    error.classList.remove('hidden')
-    //   then add the error message
-    error.innerHTML = `<p>${data.message}</p>`
+    const response = await fetch(`/user/${userId}/url`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        url: urlValue,
+        slug: slugValue || undefined,
+      }),
+    })
+    if (response.ok) {
+      console.log('response worked')
+      const data = await response.json()
+      console.log('data', data)
+      // append new url to the page to open in a new tab
+      newUrl.innerHTML = `<p><a href="${data.slug}" target="_blank">${window.location.origin}/${data.slug}</a></p>`
+    } else {
+      const data = await response.json()
+      //   first remove .hidden class
+      error.classList.remove('hidden')
+      //   then add the error message
+      error.innerHTML = `<p>${data.message}</p>`
+    }
   }
 }
